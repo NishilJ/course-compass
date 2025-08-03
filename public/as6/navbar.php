920b51a59343b8ae90a463e022538050aaa16420
NishilJ <78994772+NishilJ@users.noreply.github.com>
2025-08-03 13:53:05 -0500

    move as6 files

---
+++ public/as6/navbar.php
@@ -0,0 +1,30 @@
+<!-- navbar.php -->
+<style>
+    .navbar {
+        background-color: #f2f2f2;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 20px;
+        font-family: sans-serif;
+    }
+
+    .navbar a {
+        text-decoration: none;
+        color: #333;
+        padding: 8px 16px;
+        margin-right: 10px;
+        border-radius: 5px;
+        transition: background-color 0.3s ease;
+    }
+
+    .navbar a:hover {
+        background-color: #ddd;
+    }
+</style>
+
+<div class="navbar">
+    <a href="http://localhost:8080/select-injection.php">Select Injection </a>
+    <a href="http://localhost:8080/update-injection.php">Update Injection</a>
+    <a href="http://localhost:8080/update-injection2.php">Update Injection 2</a>
+    <a href="http://localhost:8080/prepared-statement.php">Prepared Statement</a>
+</div>
