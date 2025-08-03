4b58a5d1eef2377c75d9f67c1af5a5ff7236ca19
NishilJ <78994772+NishilJ@users.noreply.github.com>
2025-08-03 14:16:14 -0500

    Update navbar.php

--- public/navbar.php
+++ public/navbar.php
@@ -1,21 +1,22 @@
 <div class="nav-bar">
     <div class="left-section">
-      <span class="title">
-        <a href="index.php">Course Compass</a>
-      </span>
+        <span class="title">
+            <a href="index.php">Course Compass</a>
+        </span>
     </div>
     <div class="logo">
-      <img src="assets/images/utd-logo.svg" alt="Logo" class="logo-img">
+        <img src="assets/images/utd-logo.svg" alt="Logo" class="logo-img">
     </div>
     <div class="right-section">
-      <div class="dropdown">
-        <div class="icon">
-          <i class="dropbtn material-icons">menu</i>
+        <div class="dropdown">
+            <div class="icon">
+                <i class="dropbtn material-icons">menu</i>
+            </div>
+            <div class="dropdown-content">
+                <a href="index.php">Home</a>
+                <a href="admin-login.php">Admin</a>
+                <a href="?logout=1">Log Out</a>
+            </div>
         </div>
-        <div class="dropdown-content">
-          <a href="index.php">Home</a>
-          <a href="admin-login.php">Admin</a>
-        </div>
-      </div>
     </div>
 </div>
